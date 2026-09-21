ab0d11d00c1414069e4bd6652de81ece92ddd420
jeanpiercordovacordova-arch <jeanpiercordovacordova@gmail.com>
2026-09-21 17:50:07 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ function activateExperience() {
 }
 
 function showPersonalisation(name, word) {
-  personalGreeting.textContent = `Para Cintia`;
+  personalGreeting.textContent = `Para Cintia `;
   const heart = document.createElement('em');
   heart.textContent = '♡';
   personalGreeting.append(heart);
